c255a9d7069c580360c9a0a4ead3c23a9bf3e7c4
Mamun05Rashid <mamun05rashid@gmail.com>
2023-09-13 22:55:17 +0600

    new

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// @type {import('tailwindcss').Config}
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+      colors:{
+        brand: "#F85559"
+      }
+    },
+    plugins: [],
+  }
